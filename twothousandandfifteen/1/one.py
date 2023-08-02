9d927c40b9bc6af38f9cbb9b9425f24a855732e5
Tim Neufeld <>
2023-08-02 13:28:47 +0200

    solve 2015/1 and configure 2015/2

--- twothousandandfifteen/1/one.py
+++ twothousandandfifteen/1/one.py
@@ -1,1 +1,21 @@
-print('wow')
+floor = 0
+negative = False
+character_count = 0
+
+input = open('input.txt')
+input = input.read()
+
+for character in input:
+    if character == '(':
+        floor = floor + 1
+    elif character == ')':
+        floor = floor - 1
+
+    if negative == False:
+        character_count = character_count + 1
+
+    if floor < 0:
+        negative = True
+
+print('position of first negative floor: ' + str(character_count))
+print('floor: ' + str(floor))
